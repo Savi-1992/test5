fe28cbc78eb27e816120272d8ad9a97a8626fd30
Savi-1992 <ssavitadandare@gmail.com>
2025-03-17 09:20:53 +0530

    Update Test5Application.java

--- src/main/java/com/test5/Test5Application.java
+++ src/main/java/com/test5/Test5Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test5Application {
 
 	public static void main(String[] args) {
-		Spring name ="mike";
+		Spring name ="Mike";
 		SpringApplication.run(Test5Application.class, args);
 	}
 
